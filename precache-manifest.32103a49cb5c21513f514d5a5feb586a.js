1933d214067e6f23b8630bd67c8d4b28b3547f8f
Rinku Vipin Mathews <rinkuvipinmathews@gmail.com>
2020-10-22 22:52:17 +0530

    Updates

---
+++ precache-manifest.32103a49cb5c21513f514d5a5feb586a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5cb39955a1a41a916f2af1e7ee5107",
+    "url": "/RinkuVipin/shopping-list/index.html"
+  },
+  {
+    "revision": "e51dd045c39443e30006",
+    "url": "/RinkuVipin/shopping-list/static/css/main.1a50abcd.chunk.css"
+  },
+  {
+    "revision": "0bd0e5a05273147144cc",
+    "url": "/RinkuVipin/shopping-list/static/js/2.6d240a5a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/RinkuVipin/shopping-list/static/js/2.6d240a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51dd045c39443e30006",
+    "url": "/RinkuVipin/shopping-list/static/js/main.430ab7ba.chunk.js"
+  },
+  {
+    "revision": "e7332139b4a5b5857dd9",
+    "url": "/RinkuVipin/shopping-list/static/js/runtime-main.e0143bc2.js"
+  }
+]);
